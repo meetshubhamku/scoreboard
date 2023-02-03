73af2ee7a2c73e7ab157e190d7420a038aa7a6c8
Shubham <meetshubhamku@gmail.com>
2023-02-03 16:51:37 +0530

    test

--- src/App/App.js
+++ src/App/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { MdSportsCricket } from "react-icons/md";
 import { FiTarget } from "react-icons/fi";
 import { AiTwotoneQuestionCircle } from "react-icons/ai";
@@ -10,11 +10,11 @@ export default function App() {
   };
 
   const [data, setData] = useState({
-    teamName: "Africa",
-    teamScore: 50,
-    wicket: 7,
+    teamName: "India",
+    teamScore: 0,
+    wicket: 0,
     balls: "0.0",
-    target: 500,
+    target: "",
     message: "",
   });
 
@@ -40,189 +40,201 @@ export default function App() {
     // e.preventDefault();
   };
 
-  return (
-    <>
-      {data && (
-        <div className="container-fluid">
-          <div className="row">
-            <div
-              className="col-12 text-dark py-2 mx-auto "
-              style={{ backgroundColor: "black" }}
-            >
-              <p className="border border-dark rounded-pill text-center pb-3 mb-4 mt-2 pt-1 bg-light col-6 mx-auto">
-                <i
-                  className="text-danger my-0 py-0"
-                  style={{
-                    fontSize: "15px",
-                  }}
-                >
-                  <AiTwotoneQuestionCircle className="blink"></AiTwotoneQuestionCircle>
-                  <span className="pl-2 font-weight-bold">LIVE</span>
-                </i>
-                <p className="my-0">
-                  <span className="border border-light px-2 py-3 ">
-                    <small
-                      className="font-weight-bold"
-                      style={{
-                        fontSize: "40px",
-                      }}
-                    >
-                      <i className="pr-2 text-info">
-                        <MdSportsCricket
-                          style={{
-                            fontSize: "35px",
-                          }}
-                        ></MdSportsCricket>
-                      </i>
-                      {data.teamName}
-                    </small>{" "}
-                    <small
-                      className=""
-                      style={{
-                        fontSize: "30px",
-                      }}
-                    >
-                      {" "}
-                      : {data.teamScore}/{data.wicket} ({data.balls})
-                    </small>
-                  </span>{" "}
-                  <span className="border border-light px-4 py-3">
-                    {data.target && (
-                      <small>
-                        <b
-                          style={{
-                            fontSize: "40px",
-                          }}
-                          className="ml-2"
-                        >
-                          <i
-                            className="pr-2 text-danger"
+  const UI = () => {
+    return (
+      <>
+        {data.teamName && (
+          <div className="container-fluid">
+            <div className="row">
+              <div
+                className="col-12 text-dark py-2 mx-auto "
+                style={{ backgroundColor: "black" }}
+              >
+                <p className="border border-dark rounded-pill text-center pb-3 mb-4 mt-2 pt-1 bg-light col-6 mx-auto">
+                  <i
+                    className="text-danger my-0 py-0"
+                    style={{
+                      fontSize: "15px",
+                    }}
+                  >
+                    <AiTwotoneQuestionCircle className="blink"></AiTwotoneQuestionCircle>
+                    <span className="pl-2 font-weight-bold">LIVE</span>
+                  </i>
+                  <p className="my-0">
+                    <span className="border border-light px-2 py-3 ">
+                      <small
+                        className="font-weight-bold"
+                        style={{
+                          fontSize: "40px",
+                        }}
+                      >
+                        <i className="pr-2 text-info">
+                          <MdSportsCricket
+                            style={{
+                              fontSize: "35px",
+                            }}
+                          ></MdSportsCricket>
+                        </i>
+                        {data.teamName}
+                      </small>{" "}
+                      <small
+                        className=""
+                        style={{
+                          fontSize: "30px",
+                        }}
+                      >
+                        {" "}
+                        : {data.teamScore}/{data.wicket} ({data.balls})
+                      </small>
+                    </span>{" "}
+                    <span className="border border-light px-4 py-3">
+                      {data.target && (
+                        <small>
+                          <b
                             style={{
                               fontSize: "40px",
                             }}
+                            className="ml-2"
                           >
-                            <FiTarget></FiTarget>
-                          </i>
-                          Target
-                        </b>{" "}
-                        <small
-                          className=""
-                          style={{
-                            fontSize: "37px",
-                          }}
-                        >
-                          {" "}
-                          : {data.target}
+                            <i
+                              className="pr-2 text-danger"
+                              style={{
+                                fontSize: "40px",
+                              }}
+                            >
+                              <FiTarget></FiTarget>
+                            </i>
+                            Target
+                          </b>{" "}
+                          <small
+                            className=""
+                            style={{
+                              fontSize: "37px",
+                            }}
+                          >
+                            {" "}
+                            : {data.target}
+                          </small>
                         </small>
-                      </small>
-                    )}
-                  </span>
-                </p>
-
-                {data.message && (
-                  <p className="py-1 my-1">
-                    <small
-                      style={{ fontSize: "20px" }}
-                      className="font-weight-bold"
-                    >
-                      {data.message}
-                    </small>
+                      )}
+                    </span>
                   </p>
-                )}
-              </p>
+
+                  {data.message && (
+                    <p className="py-1 my-1">
+                      <small
+                        style={{ fontSize: "20px" }}
+                        className="font-weight-bold"
+                      >
+                        {data.message}
+                      </small>
+                    </p>
+                  )}
+                </p>
+              </div>
             </div>
           </div>
+        )}
+
+        <div className="container mb-5" style={{ marginTop: "200px" }}>
+          <form className="">
+            <div className="row">
+              <div className="col-3">
+                <div class="form-group">
+                  <label for="exampleInputEmail1">Team Name</label>
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="exampleInputEmail1"
+                    aria-describedby="emailHelp"
+                    value={newData.teamName}
+                    onChange={handleChange("teamName")}
+                  />
+                </div>
+              </div>
+              <div className="col-3">
+                <div class="form-group">
+                  <label for="exampleInputPassword1">Runs</label>
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="exampleInputPassword1"
+                    value={newData.teamScore}
+                    onChange={handleChange("teamScore")}
+                  />
+                </div>
+              </div>
+              <div className="col-3">
+                <div class="form-group">
+                  <label for="exampleInputPassword1">Wicket</label>
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="exampleInputPassword1"
+                    value={newData.Wicket}
+                    onChange={handleChange("Wicket")}
+                  />
+                </div>
+              </div>
+
+              <div className="col-3">
+                <div class="form-group">
+                  <label for="exampleInputPassword1">Over</label>
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="exampleInputPassword1"
+                    value={newData.over}
+                    onChange={handleChange("over")}
+                  />
+                </div>
+              </div>
+              <div className="col-3">
+                <div class="form-group">
+                  <label for="exampleInputPassword1">Target</label>
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="exampleInputPassword1"
+                    value={newData.target}
+                    onChange={handleChange("target")}
+                  />
+                </div>
+              </div>
+              <div className="col-9">
+                <div class="form-group">
+                  <label for="exampleInputPassword1">
+                    {JSON.parse(localStorage.getItem("obj"))?.message}
+                  </label>
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="exampleInputPassword1"
+                    // value={JSON.parse(localStorage.getItem("obj"))?.message}
+                    onChange={handleChange("message")}
+                  />
+                </div>
+              </div>
+            </div>
+
+            <button
+              type="button"
+              onClick={performUpdate}
+              class="btn btn-primary"
+            >
+              Update
+            </button>
+          </form>
         </div>
-      )}
 
-      <div className="container mb-5" style={{ marginTop: "200px" }}>
-        <form className="">
-          <div className="row">
-            <div className="col-3">
-              <div class="form-group">
-                <label for="exampleInputEmail1">Team Name</label>
-                <input
-                  type="text"
-                  class="form-control"
-                  id="exampleInputEmail1"
-                  aria-describedby="emailHelp"
-                  value={newData.teamName}
-                  onChange={handleChange("teamName")}
-                />
-              </div>
-            </div>
-            <div className="col-3">
-              <div class="form-group">
-                <label for="exampleInputPassword1">Runs</label>
-                <input
-                  type="text"
-                  class="form-control"
-                  id="exampleInputPassword1"
-                  value={newData.teamScore}
-                  onChange={handleChange("teamScore")}
-                />
-              </div>
-            </div>
-            <div className="col-3">
-              <div class="form-group">
-                <label for="exampleInputPassword1">Wicket</label>
-                <input
-                  type="text"
-                  class="form-control"
-                  id="exampleInputPassword1"
-                  value={newData.Wicket}
-                  onChange={handleChange("Wicket")}
-                />
-              </div>
-            </div>
+        <p>{localStorage.getItem("obj")}</p>
+      </>
+    );
+  };
 
-            <div className="col-3">
-              <div class="form-group">
-                <label for="exampleInputPassword1">Over</label>
-                <input
-                  type="text"
-                  class="form-control"
-                  id="exampleInputPassword1"
-                  value={newData.over}
-                  onChange={handleChange("over")}
-                />
-              </div>
-            </div>
-            <div className="col-3">
-              <div class="form-group">
-                <label for="exampleInputPassword1">Target</label>
-                <input
-                  type="text"
-                  class="form-control"
-                  id="exampleInputPassword1"
-                  value={newData.target}
-                  onChange={handleChange("target")}
-                />
-              </div>
-            </div>
-            <div className="col-9">
-              <div class="form-group">
-                <label for="exampleInputPassword1">
-                  {JSON.parse(localStorage.getItem("obj"))?.message}
-                </label>
-                <input
-                  type="text"
-                  class="form-control"
-                  id="exampleInputPassword1"
-                  // value={JSON.parse(localStorage.getItem("obj"))?.message}
-                  onChange={handleChange("message")}
-                />
-              </div>
-            </div>
-          </div>
+  useEffect(() => {
+    UI();
+  });
 
-          <button type="submit" onClick={performUpdate} class="btn btn-primary">
-            Update
-          </button>
-        </form>
-      </div>
-
-      <p>{localStorage.getItem("obj")}</p>
-    </>
-  );
+  return <>{UI()}</>;
 }
